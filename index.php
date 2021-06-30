4218a3d8bc840534054e8a23a556ede6c7b531be
dfgiordano <federico.giordano90@gmail.com>
2021-06-30 16:47:25 +0200

    aggiunta fonts

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ include 'database.php';
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Abel&family=Roboto+Mono:wght@300;700&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Abel&family=Exo+2:wght@700&family=Roboto+Mono:wght@300;700&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="./css/style.css">
         <title>Project CD php</title>
     </head>
